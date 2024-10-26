15bc6a159808819b9fdbdd38eeb23fb3351f231d
Rose Tovar <rose@vanica4rize.com>
2024-10-26 16:42:45 -0400

    added updates to controller and added associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_26_201454) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_26_203434) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
