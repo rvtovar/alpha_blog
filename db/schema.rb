3d8736a5d44c837b55ce4477c02a2d97377dd4b9
Rose Tovar <rose@vanica4rize.com>
2024-10-27 12:04:10 -0400

    added auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_26_203434) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_155949) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,5 +24,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_203434) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
